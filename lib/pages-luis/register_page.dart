f6c4b9fb2664302bd22932816f2e1af99614531f
Diego Aular <andresaular1212@gmail.com>
2026-07-05 22:19:07 -0500

    Luis: agregar carpeta pages-luis

---
+++ lib/pages-luis/register_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../common-diego/app_colors.dart';
+import '../widgets-mirian/custom_text_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Registro')),
+      body: Padding(
+        padding: const EdgeInsets.all(18),
+        child: ListView(
+          children: [
+            const Text(
+              'Crear cuenta en ClassReport',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 18),
+
+            CustomTextField(
+              controller: nameController,
+              label: 'Nombre completo',
+              icon: Icons.person_outline,
+            ),
+
+            const SizedBox(height: 12),
+
+            CustomTextField(
+              controller: emailController,
+              label: 'Correo institucional',
+              icon: Icons.email_outlined,
+            ),
+
+            const SizedBox(height: 18),
+
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Registro simulado correctamente'),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primary,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Registrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
